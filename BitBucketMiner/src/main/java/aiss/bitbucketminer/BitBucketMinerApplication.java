21eb140b8c33d59d7d59817022c05a0c77ee3d9c
dlozaco975@gmail.com <dlozaco975@gmail.com>
2025-05-03 12:08:33 +0200

    BitBucketMiner added to project

---
+++ BitBucketMiner/src/main/java/aiss/bitbucketminer/BitBucketMinerApplication.java
@@ -0,0 +1,13 @@
+package aiss.bitbucketminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitBucketMinerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitBucketMinerApplication.class, args);
+    }
+
+}
