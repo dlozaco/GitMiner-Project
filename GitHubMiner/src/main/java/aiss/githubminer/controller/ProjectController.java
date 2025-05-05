e8388e8931f764d3503547f5322d9de1e1e7d4c5
dlozaco975@gmail.com <dlozaco975@gmail.com>
2025-05-05 14:46:54 +0200

    Add GetMapping to ProjectController for retrieving project details by owner and repo

--- GitHubMiner/src/main/java/aiss/githubminer/controller/ProjectController.java
+++ GitHubMiner/src/main/java/aiss/githubminer/controller/ProjectController.java
@@ -3,15 +3,21 @@ package aiss.githubminer.controller;
 import aiss.githubminer.model.Project;
 import aiss.githubminer.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/github")
 public class ProjectController {
 
     @Autowired
     ProjectService projectService;
 
-    public Project getProject(String owner, String repo){
+    @GetMapping("/{owner}/{repo}")
+    public Project getProject(@PathVariable(value = "owner") String owner,
+                              @PathVariable(value = "repo") String repo){
         return projectService.getProject(owner, repo);
     }
 }
